335e7bd602110efb922ab0dea8480412bae04ddc
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 18:18:57 -0300

    feat: list_len function, counts how many elements there are in the specified linked list

--- srcs/node_operations.c
+++ srcs/node_operations.c
@@ -6,10 +6,26 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:54:35 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 18:09:43 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 18:17:25 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
+
+static int	list_len(t_node **head)
+{
+	int	count;
+
+	count = 0;
+	if (head)
+	{
+		while (*head)
+		{
+			*head = (*head)->next;
+			count++;
+		}
+	}
+	return (count);
+}
 
 void	node_add_back(t_node **head, t_node *node)
 {
@@ -46,7 +62,6 @@ void	del_first_node(t_node **head)
 
 void	del_last_node(t_node **head)
 {
-	t_node	*prev_node;
 	t_node	*tmp;
 
 	tmp = *head;
@@ -55,12 +70,8 @@ void	del_last_node(t_node **head)
 		if (*head != NULL)
 		{
 			while (tmp->next != NULL)
-			{
-				prev_node = tmp;
 				tmp = tmp->next;
-			}
 			free(tmp);
-			prev_node->next = NULL;
 		}
 	}
 }
