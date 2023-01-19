cf3e135696842fe143a22c8adced258b743dcc1c
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 18:10:12 -0300

    refactor: moved new_node function into separate file

--- srcs/node_operations.c
+++ srcs/node_operations.c
@@ -6,25 +6,10 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:54:35 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 17:49:49 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 18:09:43 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
-
-t_node	*new_node(int data)
-{
-	t_node	*new_node;
-
-	new_node = malloc(sizeof(t_node));
-	if (new_node == NULL)
-	{
-		write(1, "Allocation failed at new_node function\n", 39);
-		return (NULL);
-	}
-	new_node->data = data;
-	new_node->next = NULL;
-	return (new_node);
-}
 
 void	node_add_back(t_node **head, t_node *node)
 {
