4e218a2ba8f9406989f38f525bd4c3c07142eb0f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 15:34:23 -0300

    feat: new_node returns a new allocated node based on data given, node_add_back adds a node to the last element of head list

---
+++ srcs/node_operations.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   node_operations.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 14:54:35 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/01/19 15:33:03 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/push_swap.h"
+
+t_node	*new_node(int data)
+{
+	t_node	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	if (new_node == NULL)
+	{
+		write(1, "Allocation failed at new_node function\n", 39);
+		return (NULL);
+	}
+	new_node->data = data;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	node_add_back(t_node **head, t_node *node)
+{
+	t_node	*tmp;
+
+	tmp = *head;
+	if (head != NULL && node != NULL)
+	{
+		if (*head == NULL)
+			*head = node;
+		else
+		{
+			while (tmp->next != NULL)
+				tmp = tmp->next;
+			tmp->next = node;
+		}
+	}
+}
