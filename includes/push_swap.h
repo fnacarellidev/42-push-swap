3b21cd6ba23a44dc561155c98d4f96371c254618
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 15:35:22 -0300

    feat: including stdlib.h and unistd.h, also prototyping new functions added on srcs/node_operations.c

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,16 +6,21 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:13:21 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 14:14:36 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 15:10:59 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_node
 {
 	int				data;
 	struct s_node	*next;
 }	t_node;
+
+t_node	*new_node(int data);
+void	node_add_back(t_node **head, t_node *node);
 
 #endif
