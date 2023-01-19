6ca0c2f79f66d0d21efd07c54cbc1211bb57d268
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 14:15:45 -0300

    feat: t_node struct

---
+++ includes/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/19 14:13:21 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/01/19 14:14:36 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_node
+{
+	int				data;
+	struct s_node	*next;
+}	t_node;
+
+#endif
