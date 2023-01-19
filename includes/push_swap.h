6dc4c42ef4623c4fd2c7ef0b59e3fb7e731b0b6c
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 20:06:30 -0300

    feat: init_stacks and ft_atoi prototypes

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:13:21 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 18:34:05 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 19:54:42 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
@@ -27,5 +27,7 @@ void	del_first_node(t_node **head);
 void	node_add_back(t_node **head, t_node *node);
 void	node_add_front(t_node **head, t_node *node);
 void	free_list(t_node **head);
+int		ft_atoi(const char *nptr);
+void	init_stacks(t_node ***stack_a, t_node ***stack_b, int argc, char **argv);
 
 #endif
