2a3abd63e495962e6b4e89dd4916656be266178f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 19:07:13 -0300

    feat: free list and list len functions, free list frees my entire linked list if needed, list len give me the amount of elements in the linked list

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:13:21 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 17:47:30 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 18:34:05 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
@@ -21,9 +21,11 @@ typedef struct s_node
 }	t_node;
 
 t_node	*new_node(int data);
+int		list_len(t_node **head);
+void	del_last_node(t_node **head);
+void	del_first_node(t_node **head);
 void	node_add_back(t_node **head, t_node *node);
 void	node_add_front(t_node **head, t_node *node);
-void	del_first_node(t_node **head);
-void	del_last_node(t_node **head);
+void	free_list(t_node **head);
 
 #endif
