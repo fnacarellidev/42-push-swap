d7f3588f6bd393b0ea838d67527309129afceca3
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-20 11:18:42 -0300

    feat: rev_rotate_a
    this function gets the last element on the stack_a and puts it in the top of the stack, shifting all elements down by 1

---
+++ srcs/reverse_rotate_operations.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate_operations.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 10:28:28 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/01/20 10:41:11 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/push_swap.h"
+
+void	rev_rotate_a(t_node **stack_a)
+{
+	t_node	*tmp;
+	int		elements;
+	t_node	*last_node;
+
+	elements = list_len(stack_a);
+	if (elements < 2)
+	{
+		write(1, "Can't revese rotate stack_a, it has less than two elements.\n", 60);
+		return ;
+	}
+	tmp = *stack_a;
+	while (tmp->next->next != NULL)
+		tmp = tmp->next;
+	last_node = tmp->next;
+	tmp->next = NULL;
+	node_add_front(stack_a, last_node);
+}
