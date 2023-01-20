94c960b31890360ba4d5a68af70aec4e69be0929
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 23:14:33 -0300

    feat: push_a, this function checks if stack_b is empty, if it's not empty, it takes it's first element and puts into stack_a

--- srcs/push_operations.c
+++ srcs/push_operations.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:48:04 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 22:50:01 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 23:11:55 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -14,4 +14,20 @@
 int		list_is_empty(int elements)
 {
 	return (elements);
+}
+
+void	push_a(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*copy;
+	int		elements;
+
+	elements = list_len(stack_b);
+	if (!list_is_empty(elements))
+	{
+		write(1, "Can't push_a, stack_b is empty.\n", 32);
+		return ;
+	}
+	copy = new_node((*stack_b)->data);
+	del_first_node(stack_b);
+	node_add_front(stack_a, copy);
 }
