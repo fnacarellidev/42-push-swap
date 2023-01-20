c941ff489bc2fc324beb551911c916a70ee59c16
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-20 09:45:15 -0300

    feat: rotate_b function
    this function takes the first element of the stack_b and makes it the last one 'shifting' all elements 1 time

--- srcs/rotate_operations.c
+++ srcs/rotate_operations.c
@@ -27,3 +27,20 @@ void	rotate_a(t_node **stack_a)
 	first_node->next = NULL;
 	node_add_back(stack_a, first_node);
 }
+
+void	rotate_b(t_node **stack_b)
+{
+	int		elements;
+	t_node	*first_node;
+
+	elements = list_len(stack_b);
+	if (elements < 2)
+	{
+		write(1, "Can't rotate_b, stack_b has less than two elements.\n", 52);
+		return ;
+	}
+	first_node = *stack_b;
+	*stack_b = (*stack_b)->next;
+	first_node->next = NULL;
+	node_add_back(stack_b, first_node);
+}
