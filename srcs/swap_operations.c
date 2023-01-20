48a237c903df972be41378dbf8b1e9614b2227b7
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-19 22:41:37 -0300

    feat: swap_b, this function swaps the first two elements of the linked list stack_b

--- srcs/swap_operations.c
+++ srcs/swap_operations.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 22:11:35 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/19 22:25:42 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/19 22:39:08 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -33,4 +33,18 @@ void	swap_a(t_node **stack_a)
 		return ;
 	}
 	swap_first_two_nodes(stack_a);
+}
+
+void	swap_b(t_node **stack_b)
+{
+	int	elements;
+
+	elements = list_len(stack_b);
+	if (elements < 2)
+	{
+		write(1, "Couldn't swap stack_b, should \
+				have at least 2 elements.\n", 56);
+		return ;
+	}
+	swap_first_two_nodes(stack_b);
 }
