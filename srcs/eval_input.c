7cd82e1e69ae3472f18ae745d293c1d5e1719fd7
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-01-26 02:33:37 -0300

    feat: has_non_integer checks if the argv has any value that isn't an integer

--- srcs/eval_input.c
+++ srcs/eval_input.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 21:56:23 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/25 22:14:56 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/01/26 02:26:49 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -19,8 +19,26 @@ static int	has_input(int argc)
 }
 
 static int	power(int base, int exponent)
+static int	has_non_integer(char **argv)
 {
 	if (exponent == 1)
 		return (base);
 	return (base * power(base, exponent - 1));
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
 }
