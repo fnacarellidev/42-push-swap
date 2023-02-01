6bb190481cf5c5e6dfb410745d4e75aada26977b
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-01 19:07:07 -0300

    perf: when min val is on last index, just perform rra

--- srcs/sort_four.c
+++ srcs/sort_four.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 18:15:56 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/01 18:29:47 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/01 19:03:19 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -30,10 +30,19 @@ static int	get_min_val(t_node **stack)
 void	sort_four(t_node **stack_a, t_node **stack_b)
 {
 	int	min;
+	int	last_index;
+	int	min_val_index;
 
-	min = get_min_val(stack_a);
-	while ((*stack_a)->data != min)
-		rotate_a(stack_a);
+	last_index = list_len(stack_a) - 1;
+	min_val_index = get_min_val_index(stack_a);
+	min = get_min_val(stack_a, min_val_index);
+	if (min_val_index == last_index)
+		rev_rotate_a(stack_a);
+	else
+	{
+		while ((*stack_a)->data != min)
+			rotate_a(stack_a);
+	}
 	push_b(stack_a, stack_b);
 	sort_three(stack_a);
 	push_a(stack_a, stack_b);
