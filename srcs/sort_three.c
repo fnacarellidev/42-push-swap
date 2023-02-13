31067344427eebf5bbe50cd35a08f645207eeca7
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-13 15:07:03 -0300

    refactor: just some renaming, function does the same

--- srcs/sort_three.c
+++ srcs/sort_three.c
@@ -6,41 +6,41 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 17:40:27 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/01/27 18:23:03 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/13 15:02:01 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
 
-static void	which_move_to_do(t_node **stack, int data1, int data2, int data3)
+static void	which_move_to_do(t_node **stack, int idx1, int idx2, int idx3)
 {
-	if (data1 > data2 && data2 < data3 && data1 < data3)
-		swap_a(stack);
-	else if (data1 > data2 && data2 > data3)
+	if ((idx1 < idx2) && (idx1 < idx3))
 	{
-		swap_a(stack);
 		rev_rotate_a(stack);
-	}
-	else if (data1 > data2 && data2 < data3)
-		rotate_a(stack);
-	else if (data1 < data2 && data2 > data3 && data1 < data3)
-	{
 		swap_a(stack);
+	}
+	else if ((idx2 < idx1) && (idx2 < idx3) && (idx1 < idx3))
+		swap_a(stack);
+	else if ((idx3 < idx1) && (idx3 < idx2) && (idx1 < idx2))
+		rev_rotate_a(stack);
+	else if ((idx3 < idx2) && (idx3 < idx1))
+	{
 		rotate_a(stack);
+		swap_a(stack);
 	}
 	else
-		rev_rotate_a(stack);
+		rotate_a(stack);
 }
 
 void	sort_three(t_node **stack)
 {
-	int	data1;
-	int	data2;
-	int	data3;
+	int first_idx;
+	int second_idx;
+	int third_idx;
 
 	if (is_sorted(*stack))
 		return ;
-	data1 = (*stack)->data;
-	data2 = (*stack)->next->data;
-	data3 = (*stack)->next->next->data;
-	which_move_to_do(stack, data1, data2, data3);
+	first_idx = (*stack)->idx;
+	second_idx = (*stack)->next->idx;
+	third_idx = (*stack)->next->next->idx;
+	which_move_to_do(stack, first_idx, second_idx, third_idx);
 }
