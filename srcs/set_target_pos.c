4dba2a98ecf1371aac3d9c703be195827a84b542
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-14 05:18:34 -0300

    fix: set_target_pos was not iterating in the linked list, now it is

--- srcs/set_target_pos.c
+++ srcs/set_target_pos.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 22:58:13 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/13 22:58:47 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/14 05:15:16 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -25,14 +25,20 @@ static int	get_closest_superior_idx(t_node *stack_a, t_node *node_b)
 	return (closest_superior_idx);
 }
 
-void	set_target_pos(t_node **stack_a, t_node *node_b)
+void	set_target_pos(t_node **stack_a, t_node *stack_b)
 {
+	t_node	*tmp;
 	t_node	*head;
 	int		closest_superior_idx;
 
-	head = *stack_a;
-	closest_superior_idx = get_closest_superior_idx(*stack_a, node_b);
-	while (head->idx != closest_superior_idx)
-		head = head->next;
-	node_b->target_pos = head->curr_pos;
+	tmp = stack_b;
+	while (tmp != NULL)
+	{
+		head = *stack_a;
+		closest_superior_idx = get_closest_superior_idx(*stack_a, tmp);
+		while (head->idx != closest_superior_idx)
+			head = head->next;
+		tmp->target_pos = head->curr_pos;
+		tmp = tmp->next;
+	}
 }
