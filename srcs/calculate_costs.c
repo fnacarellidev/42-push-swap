e5974c1a1cf737025a9a719b4f2bfc8ff6aeb929
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-14 05:20:24 -0300

    feat: calculates the number of moves to put certain element of stack_a on the top

---
+++ srcs/calculate_costs.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calculate_costs.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 23:44:40 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/02/14 05:01:15 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/push_swap.h"
+
+static int	calc_cost_a(t_node *stack_a, t_node *node_b)
+{
+	int	size_stack_a;
+
+	size_stack_a = list_len(&stack_a);
+	if (node_b->target_pos <= size_stack_a / 2)
+		return (node_b->target_pos);
+	return (node_b->target_pos - size_stack_a);
+}
