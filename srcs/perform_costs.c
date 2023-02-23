8ce1313cac84f811f2d9b89b0fb11b4c82af2c5f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-23 20:34:59 -0300

    refactor: get position with cheapest cost

--- srcs/perform_costs.c
+++ srcs/perform_costs.c
@@ -18,12 +18,25 @@ static int	ft_abs(int i)
 	return (i);
 }
 
+int		get_position_with_cheapest_cost(t_node *stack_b)
 {
+	int	cheapest_cost;
+	int	cost_to_compare;
+	int	cheapest_cost_pos;
 
+	cheapest_cost_pos = 0;
+	cheapest_cost = ft_abs(stack_b->cost_a) + ft_abs(stack_b->cost_b);
+	while (stack_b != NULL)
 	{
+		cost_to_compare = ft_abs(stack_b->cost_a) + ft_abs(stack_b->cost_b);
+		if (cost_to_compare < cheapest_cost)
 		{
+			cheapest_cost = cost_to_compare;
+			cheapest_cost_pos = stack_b->curr_pos;
 		}
+		stack_b = stack_b->next;
 	}
+	return (cheapest_cost_pos);
 }
 
 void	perform_cheapest_action(t_node **stack_a, t_node **stack_b)
