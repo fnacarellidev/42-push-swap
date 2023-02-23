d9382660959b495b412ffd25902a3c496a69a9cb
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-23 20:38:45 -0300

    refactor: get_position_with_cheapest_cost and set_costs got moved to a different file

--- srcs/calculate_costs.c
+++ srcs/calculate_costs.c
@@ -30,37 +30,3 @@ int	calc_cost_b(t_node *stack_b, t_node *node_b)
 		return (node_b->curr_pos);
 	return (node_b->curr_pos - size_stack_b);
 }
-
-int		get_position_with_cheapest_cost(t_node *stack_b)
-{
-	int	cheapest_cost;
-	int	cost_to_compare;
-	int	cheapest_cost_pos;
-
-	cheapest_cost_pos = 0;
-	cheapest_cost = ft_abs(stack_b->cost_a) + ft_abs(stack_b->cost_b);
-	while (stack_b != NULL)
-	{
-		cost_to_compare = ft_abs(stack_b->cost_a) + ft_abs(stack_b->cost_b);
-		if (cost_to_compare < cheapest_cost)
-		{
-			cheapest_cost = cost_to_compare;
-			cheapest_cost_pos = stack_b->curr_pos;
-		}
-		stack_b = stack_b->next;
-	}
-	return (cheapest_cost_pos);
-}
-
-void	set_costs(t_node *stack_a, t_node *stack_b)
-{
-	t_node	*tmp;
-
-	tmp = stack_b;
-	while (tmp != NULL)
-	{
-		tmp->cost_a = calc_cost_a(stack_a, tmp);
-		tmp->cost_b = calc_cost_b(stack_b, tmp);
-		tmp = tmp->next;
-	}
-}
