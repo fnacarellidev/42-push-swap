a21d201a68471618c31d0890698f968e7dc29ef2
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-24 16:56:17 -0300

    refactor: moved ft_has_only_sign from param_parsing to ft_has_only_sign

--- srcs/utils/ft_has_only_sign.c
+++ srcs/utils/ft_has_only_sign.c
@@ -1,11 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   param_parsing.c                                    :+:      :+:    :+:   */
+/*   ft_has_only_sign.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/26 15:36:18 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/24 16:55:42 by fnacarel         ###   ########.fr       */
+/*   Created: 2023/02/24 16:55:20 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/02/24 16:55:36 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "../../includes/push_swap.h"
+
+int	ft_has_only_sign(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_issign(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
