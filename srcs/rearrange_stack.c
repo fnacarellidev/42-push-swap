35bf922984b17d9aa8d19049ac7e451e39e1d732
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-24 14:50:37 -0300

    fix: now smallest_idx is an static function

--- srcs/rearrange_stack.c
+++ srcs/rearrange_stack.c
@@ -6,12 +6,12 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:22:07 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/23 20:16:04 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/24 14:35:34 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
 
-int		smallest_idx_current_position(t_node *stack)
+static int	smallest_idx_current_position(t_node *stack)
 {
 	while (stack->idx != 1)
 		stack = stack->next;
