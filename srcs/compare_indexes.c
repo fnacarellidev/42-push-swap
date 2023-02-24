41b85921a11d496da1ad2355215e496e03f51b0e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-24 13:38:53 -0300

    refactor: have_bigger_idx was an unused function

--- srcs/compare_indexes.c
+++ srcs/compare_indexes.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:56:59 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/23 14:57:56 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/24 13:35:49 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -16,17 +16,6 @@ int		have_smaller_idx(int idx, t_node *stack)
 	while (stack != NULL)
 	{
 		if (stack->idx < idx)
-			return (1);
-		stack = stack->next;
-	}
-	return (0);
-}
-
-int		have_bigger_idx(int idx, t_node *stack)
-{
-	while (stack != NULL)
-	{
-		if (stack->idx > idx)
 			return (1);
 		stack = stack->next;
 	}
