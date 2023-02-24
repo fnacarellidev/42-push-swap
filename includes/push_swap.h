65ee11f0bf4263c822c2a60165ee2b805d20971f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-24 13:38:20 -0300

    refactor: no longer using have_bigger_idx

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:13:21 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/24 13:03:42 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/24 13:35:42 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
@@ -35,7 +35,6 @@ int			is_sorted(t_node *stack);
 int			ft_atoi(const char *nptr);
 int			ft_has_only_sign(char *str);
 int			ft_power(int base, int exponent);
-int			have_bigger_idx(int idx, t_node *stack);
 int			have_smaller_idx(int idx, t_node *stack);
 int			calc_cost_a(t_node *stack_a, t_node *node_b);
 int			calc_cost_b(t_node *stack_a, t_node *node_b);
