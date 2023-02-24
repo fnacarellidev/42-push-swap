87b46d00319d07de525ffd87d76cabe120be8e25
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-24 16:51:45 -0300

    refactor: ft_isnumber moved from param_parsing to ft_isnumber

--- srcs/utils/ft_isnumber.c
+++ srcs/utils/ft_isnumber.c
@@ -1,39 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   param_parsing.c                                    :+:      :+:    :+:   */
+/*   ft_isnumber.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/26 15:36:18 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/24 16:49:47 by fnacarel         ###   ########.fr       */
+/*   Created: 2023/02/24 16:48:55 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/02/24 16:49:15 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_issign(char c)
+int	ft_isnumber(char c)
 {
-	if (c == 43 || c == 45)
+	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
-}
-
-
-int	ft_isspace(char c)
-{
-	if (c == 32)
-		return (1);
-	return (0);
-}
-
-int	ft_has_only_sign(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (!ft_issign(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
 }
