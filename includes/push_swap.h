0033ad4945158104890453cb501c9877052b27ab
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-23 21:05:10 -0300

    feat: solve both prototype added to header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -72,6 +72,7 @@ void		set_positions(t_node *stack_a, t_node *stack_b);
 void		set_target_pos(t_node **stack_a, t_node *node_b);
 void		sort(t_node **stack_a, t_node **stack_b, int size_a);
 void		perform_cheapest_action(t_node **stack_a, t_node **stack_b);
+void		solve_both(t_node **stack_a, int *cost_a, t_node **stack_b, int *cost_b);
 void		init_stacks(t_node ***stack_a, t_node ***stack_b, int argc, char **argv);
 long int	ft_latoi(const char *nptr);
 
