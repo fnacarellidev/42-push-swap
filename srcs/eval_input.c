b27bc9ed3bc0c1f0ec3581441ec76513d0a006e2
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-25 18:45:33 -0300

    fix: removed error message when argc < 2

--- srcs/eval_input.c
+++ srcs/eval_input.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 21:56:23 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/24 14:30:25 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/25 18:44:05 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
@@ -102,10 +102,7 @@ static int	has_duplicate(char **argv)
 void	eval_input(int argc, char **argv)
 {
 	if (argc < 2)
-	{
-		write(1, "Error\n", 6);
 		exit(1);
-	}
 	if (has_non_integer(argv))
 	{
 		write(1, "Error\n", 6);
