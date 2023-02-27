5df59c79721ebb1c53a920326b943d9069b565bc
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 14:43:49 -0300

    feat: adding linked_list struct to bonus header

--- bonus/includes/checker_bonus.h
+++ bonus/includes/checker_bonus.h
@@ -6,12 +6,18 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:44:22 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 11:48:29 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 14:32:10 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef CHECKER_BONUS_H
 # define CHECKER_BONUS_H
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_node
+{
+	int				data;
+	struct s_node	*next;
+}	t_node;
 
 #endif
