c6f7da446e21b0d5f231d6287b76236d1821784e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 18:43:41 -0300

    refactor: moved ft_count_matrix as an external function, renamed it to matrix_len

--- bonus/srcs/utils/ft_free_matrix_bonus.c
+++ bonus/srcs/utils/ft_free_matrix_bonus.c
@@ -6,29 +6,19 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:52:08 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 16:52:23 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:23:17 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/checker_bonus.h"
 
-static int	ft_count_matrix(void **matrix)
-{
-	int	i;
-
-	i = 0;
-	while (matrix[i])
-		i++;
-	return (i);
-}
-
 void	ft_free_matrix(void **matrix)
 {
 	int	i;
-	int	matrix_len;
+	int	matrix_size;
 
 	i = 0;
-	matrix_len = ft_count_matrix(matrix);
-	while (i < matrix_len)
+	matrix_size = matrix_len(matrix);
+	while (i < matrix_size)
 	{
 		free(matrix[i]);
 		i++;
