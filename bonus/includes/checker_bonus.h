2b298ae7740438b45ee7a329ce16679692266a9e
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 17:05:43 -0300

    feat: ft_strlen prototype added to header

--- bonus/includes/checker_bonus.h
+++ bonus/includes/checker_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:44:22 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 14:32:10 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 16:53:50 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef CHECKER_BONUS_H
@@ -20,4 +20,5 @@ typedef struct s_node
 	struct s_node	*next;
 }	t_node;
 
+int		ft_strlen(char *str);
 #endif
