3b60dd70ae62e66671f4fd5dd4ee797ed1752afa
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 18:44:07 -0300

    refactor: removed useless header include

--- mandatory/srcs/utils/ft_has_only_space.c
+++ mandatory/srcs/utils/ft_has_only_space.c
@@ -6,11 +6,9 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:53:50 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 11:55:00 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:21:09 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "../../includes/push_swap.h"
-
 int	ft_has_only_space(char *str)
 {
 	while (*str)
