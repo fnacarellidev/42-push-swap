39e19ea3461818d03258f9b1a487d61fad1180df
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 11:32:25 -0300

    refactor: added every file that is related to the mandatory part of the project to the mandatory folder

--- mandatory/srcs/push_swap.c
+++ mandatory/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 15:09:52 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/25 18:44:09 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 11:31:45 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/push_swap.h"
