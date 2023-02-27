d4f39b4fb39dfe21ef82a1b81e91586f5d18a8ec
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 17:05:18 -0300

    style: norminette was complaining

--- bonus/srcs/utils/ft_strlen_bonus.c
+++ bonus/srcs/utils/ft_strlen_bonus.c
@@ -6,12 +6,12 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:50:29 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 16:50:41 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 17:04:58 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/checker_bonus.h"
 
-int		ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	int	i;
 
