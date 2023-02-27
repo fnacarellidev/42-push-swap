4fbf396eb0c9788e24de3383566d5fbc74180486
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 18:44:57 -0300

    feat: a bunch of prototypes added to top of bonus header, too lazy to commit one by one

--- bonus/includes/checker_bonus.h
+++ bonus/includes/checker_bonus.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:44:22 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 16:53:50 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:30:02 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef CHECKER_BONUS_H
@@ -20,13 +20,23 @@ typedef struct s_node
 	struct s_node	*next;
 }	t_node;
 
-int		ft_strlen(char *str);
-int		list_len(t_node **head);
-int		ft_strncmp(char *s1, char *s2, int n);
-void	ft_bzero(char buf[4]);
-void	free_list(t_node **head);
-void	ft_free_matrix(void **matrix);
-void	node_add_back(t_node **head, t_node *node);
-char	*ft_strdup(char *str);
+int			ft_issign(char c);
+int			ft_isspace(char c);
+int			ft_isnumber(char c);
+int			ft_strlen(char *str);
+int			list_len(t_node **head);
+int			is_sorted(t_node *stack);
+int			matrix_len(void **matrix);
+int			ft_has_only_space(char *str);
+int			ft_power(int base, int exponent);
+int			ft_strncmp(char *s1, char *s2, int n);
+int			ft_atoi(const char *nptr);
+void		ft_bzero(char buf[4]);
+void		free_list(t_node **head);
+void		ft_free_matrix(void **matrix);
+void		eval_input(int argc, char **argv);
+void		node_add_back(t_node **head, t_node *node);
+char		*ft_strdup(char *str);
+long int	ft_latoi(const char *nptr);
 
 #endif
