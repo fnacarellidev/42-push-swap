69b2671babe706ddf178dfdc81c9db6ba55f8976
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 17:18:15 -0300

    feat: init_valid_operations initializes valid operations for push_swap

--- bonus/srcs/checker_bonus.c
+++ bonus/srcs/checker_bonus.c
@@ -30,6 +30,23 @@ int		is_valid_operation(char *instruction, char **valid_operations)
 	return (0);
 }
 
+void	init_valid_operations(char ***valid_operations)
+{
+	*valid_operations = malloc(sizeof(char *) * 12);
+	(*valid_operations)[0] = ft_strdup("sa\n");
+	(*valid_operations)[1] = ft_strdup("sb\n");
+	(*valid_operations)[2] = ft_strdup("ss\n");
+	(*valid_operations)[3] = ft_strdup("pa\n");
+	(*valid_operations)[4] = ft_strdup("pb\n");
+	(*valid_operations)[5] = ft_strdup("ra\n");
+	(*valid_operations)[6] = ft_strdup("rb\n");
+	(*valid_operations)[7] = ft_strdup("rr\n");
+	(*valid_operations)[8] = ft_strdup("rra\n");
+	(*valid_operations)[9] = ft_strdup("rrb\n");
+	(*valid_operations)[10] = ft_strdup("rrr\n");
+	(*valid_operations)[11] = NULL;
+}
+
 int main(void)
 {
 	printf("Hello World\n");
