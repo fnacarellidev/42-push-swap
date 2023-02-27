afda3c4aeff0dc52f4eefca3bcf9d2f49bf18423
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 11:49:10 -0300

    style: renaming file so it pleases 42's norm

--- bonus/includes/checker_bonus.h
+++ bonus/includes/checker_bonus.h
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
+/*   checker_bonus.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 11:44:22 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 11:45:08 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/27 11:48:29 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef CHECKER_H
-# define CHECKER_H
+#ifndef CHECKER_BONUS_H
+# define CHECKER_BONUS_H
 # include <stdlib.h>
 # include <unistd.h>
 
