2dd23329cf821e3e1f67940651f542eab187ea43
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-27 17:20:32 -0300

    feat: list_len returns the size of a linked list (bonus)

---
+++ bonus/srcs/linked_list_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   linked_list_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 14:30:32 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/02/27 14:36:11 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/checker_bonus.h"
+
+int	list_len(t_node **head)
+{
+	t_node	*tmp;
+	int		count;
+
+	count = 0;
+	if (head)
+	{
+		tmp = *head;
+		while (tmp)
+		{
+			tmp = tmp->next;
+			count++;
+		}
+	}
+	return (count);
+}
