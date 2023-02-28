2775bd3725aabad4512fbdb30465bd50b70ae7f8
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-28 17:50:59 -0300

    feat: (bonus) set_stacks for bonus

---
+++ bonus/srcs/set_stacks_bonus.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_stacks_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/28 15:30:32 by fnacarel          #+#    #+#             */
+/*   Updated: 2023/02/28 15:52:38 by fnacarel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "../includes/checker_bonus.h"
+
+static int	index_for_curr_node(t_node *head_a, t_node **node_being_analyzed)
+{
+	int	idx;
+
+	idx = 1;
+	while (head_a != NULL)
+	{
+		if (head_a->data < (*node_being_analyzed)->data)
+			idx++;
+		head_a = head_a->next;
+	}
+	return (idx);
+}
+
+static void	set_stack_indexes(t_node **stack_a)
+{
+	int		i;
+	int		list_size;
+	t_node	*node_to_assign;
+
+	i = 0;
+	node_to_assign = *stack_a;
+	list_size = list_len(stack_a);
+	while (i < list_size)
+	{
+		node_to_assign->idx = index_for_curr_node(*stack_a, &node_to_assign);
+		i++;
+		node_to_assign = node_to_assign->next;
+	}
+}
+
+void	set_stacks(t_node ***stk_a, t_node ***stk_b, int argc, char **argv)
+{
+	int		i;
+	int		data;
+	t_node	*node;
+
+	i = 1;
+	*stk_a = malloc(sizeof(t_node *));
+	*stk_b = malloc(sizeof(t_node *));
+	**stk_a = NULL;
+	**stk_b = NULL;
+	while (i < argc)
+	{
+		data = ft_atoi(argv[i]);
+		node = new_node(data);
+		node_add_back(*stk_a, node);
+		i++;
+	}
+	set_stack_indexes(*stk_a);
+}
