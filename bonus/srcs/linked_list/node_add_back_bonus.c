a1dd0f2c20058b7ebfdcc37408d9a39d789e9e5a
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-28 17:48:48 -0300

    feat: (bonus) node_add_back for opers too

--- bonus/srcs/linked_list/node_add_back_bonus.c
+++ bonus/srcs/linked_list/node_add_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:02:45 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/28 15:02:57 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/28 16:00:02 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/checker_bonus.h"
@@ -14,6 +14,24 @@
 void	node_add_back(t_node **head, t_node *node)
 {
 	t_node	*tmp;
+
+	tmp = *head;
+	if (head && node)
+	{
+		if (*head == NULL)
+			*head = node;
+		else
+		{
+			while (tmp->next != NULL)
+				tmp = tmp->next;
+			tmp->next = node;
+		}
+	}
+}
+
+void	node_add_back_oper(t_opers **head, t_opers *node)
+{
+	t_opers	*tmp;
 
 	tmp = *head;
 	if (head && node)
