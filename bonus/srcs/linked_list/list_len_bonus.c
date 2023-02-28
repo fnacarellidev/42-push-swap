d0e4fca426ee62cd6d75de9fec3b539aae367f6c
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-28 17:47:57 -0300

    feat: (bonus) oper_list_len gets the size of the opers linked list

--- bonus/srcs/linked_list/list_len_bonus.c
+++ bonus/srcs/linked_list/list_len_bonus.c
@@ -6,10 +6,28 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:03:09 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/28 15:03:31 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/28 15:56:15 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/checker_bonus.h"
+
+int	oper_list_len(t_opers **head)
+{
+	t_opers		*tmp;
+	int			count;
+
+	count = 0;
+	if (head)
+	{
+		tmp = *head;
+		while (tmp)
+		{
+			tmp = tmp->next;
+			count++;
+		}
+	}
+	return (count);
+}
 
 int	list_len(t_node **head)
 {
