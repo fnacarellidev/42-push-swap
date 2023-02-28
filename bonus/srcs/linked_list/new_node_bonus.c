7278ca38202fb3dc7eb5d674b9e1c9728c597989
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-28 17:48:26 -0300

    feat: (bonus) new_node for opers too

--- bonus/srcs/linked_list/new_node_bonus.c
+++ bonus/srcs/linked_list/new_node_bonus.c
@@ -6,19 +6,31 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:01:18 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/28 15:01:29 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/28 15:59:03 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/checker_bonus.h"
 
-t_node	*new_node(char *data)
+t_opers	*oper_new_node(char *data)
+{
+	t_opers	*new_node;
+
+	new_node = malloc(sizeof(t_node));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->data = ft_strdup(data);
+	new_node->next = NULL;
+	return (new_node);
+}
+
+t_node	*new_node(int data)
 {
 	t_node	*new_node;
 
 	new_node = malloc(sizeof(t_node));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->data = ft_strdup(data);
+	new_node->data = data;
 	new_node->next = NULL;
 	return (new_node);
 }
