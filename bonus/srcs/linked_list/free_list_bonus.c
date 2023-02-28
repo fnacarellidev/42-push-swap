356d173ed9b25776115bf155c20ca649b9b2fb10
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-02-28 17:47:17 -0300

    feat: (bonus) added some functions for freeing the t_opers linked list too

--- bonus/srcs/linked_list/free_list_bonus.c
+++ bonus/srcs/linked_list/free_list_bonus.c
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 15:01:42 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/28 15:02:30 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/02/28 16:04:31 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../includes/checker_bonus.h"
@@ -14,6 +14,31 @@
 static void	del_last_node(t_node **head)
 {
 	t_node	*tmp;
+
+	tmp = *head;
+	if (head)
+	{
+		if (*head != NULL)
+		{
+			if (tmp->next == NULL)
+			{
+				free(tmp);
+				*head = NULL;
+			}
+			else
+			{
+				while (tmp->next->next != NULL)
+					tmp = tmp->next;
+				free(tmp->next);
+				tmp->next = NULL;
+			}
+		}
+	}
+}
+
+static void	del_last_node_oper(t_opers **head)
+{
+	t_opers	*tmp;
 
 	tmp = *head;
 	if (head)
@@ -36,6 +61,21 @@ static void	del_last_node(t_node **head)
 			}
 		}
 	}
+}
+
+void	free_operation_list(t_opers **head)
+{
+	int	i;
+	int	elements;
+
+	i = 0;
+	elements = oper_list_len(head);
+	while (i < elements)
+	{
+		del_last_node_oper(head);
+		i++;
+	}
+	free(head);
 }
 
 void	free_list(t_node **head)
