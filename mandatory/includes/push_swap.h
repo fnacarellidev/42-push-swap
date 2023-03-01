0143b73af7cbfff0baeb5f35b2b855d6196081aa
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2023-03-01 12:30:47 -0300

    style: norminette was complaining about empty lines

--- mandatory/includes/push_swap.h
+++ mandatory/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 14:13:21 by fnacarel          #+#    #+#             */
-/*   Updated: 2023/02/27 14:07:35 by fnacarel         ###   ########.fr       */
+/*   Updated: 2023/03/01 12:30:34 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef PUSH_SWAP_H
@@ -24,7 +24,6 @@ typedef struct s_node
 	int				cost_b;
 	struct s_node	*next;
 }	t_node;
-
 
 t_node		*new_node(int data);
 int			ft_issign(char c);
